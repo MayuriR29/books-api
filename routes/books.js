0030cf821d81978c45e08ac7a8f91754f049292a
MayuriR29 <mayuri.gr@gmail.com>
2018-07-24 16:02:54 +0800

    added wrapper for error handling

--- routes/books.js
+++ routes/books.js
@@ -3,34 +3,40 @@ const router = express.Router();
 const Book = require("../models/book");
 const mongoose = require("mongoose");
 
+const tryWrapper = asyncMiddleware => {
+  return async (req, res, next) => {
+    try {
+      await asyncMiddleware(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
 /* GET books listing. */
-router.get("/", async (req, res, next) => {
-  try {
+router.get(
+  "/",
+  tryWrapper(async (req, res, next) => {
     const books = await Book.find().populate("author");
     res.json(books);
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
 router.get("/:id", (req, res, next) => {
   res.json({ message: `get book with id ${req.params.id}` });
 });
 
-router.post("/", async (req, res, next) => {
-  try {
+router.post(
+  "/",
+  tryWrapper(async (req, res, next) => {
     const newBook = new Book({
       title: req.body.title,
       author: req.body.author
     });
-
     await newBook.save();
-
     res.status(201).json({ message: `created a new book successfully` });
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
 router.put("/:id", (req, res, next) => {
   res.json({ message: `update book with id ${req.params.id}` });
